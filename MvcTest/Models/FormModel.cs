af140129d5285b9a8245825bf4f61b4d42cd0715
LonghronShen <LonghronShen@hotmail.com>
2018-07-02 19:01:40 +0800

    Add project files.

---
+++ MvcTest/Models/FormModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace MvcTest.Models
+{
+
+    public class FormModel
+    {
+
+        [Required]
+        public string Id { get; set; }
+
+        [Required]
+        [Display(Description = "Name")]
+        public string Name { get; set; }
+
+    }
+
+}
